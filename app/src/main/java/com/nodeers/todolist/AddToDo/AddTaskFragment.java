82e22edc7797c4dfb942571ee72118348292438a
Md Ariful Islam <34057291+ProArif@users.noreply.github.com>
2020-02-05 02:03:55 +0600

    Fragment Placement Solved

---
+++ app/src/main/java/com/nodeers/todolist/AddToDo/AddTaskFragment.java
@@ -0,0 +1,51 @@
+package com.nodeers.todolist.AddToDo;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nodeers.todolist.MainActivity;
+import com.nodeers.todolist.R;
+
+
+public class AddTaskFragment extends Fragment {
+
+
+    public AddTaskFragment() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static AddTaskFragment newInstance(String param1, String param2) {
+        AddTaskFragment fragment = new AddTaskFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_add_task, container, false);
+
+
+        return v;
+    }
+
+}
--- app/src/main/java/com/nodeers/todolist/MainActivity.java
+++ app/src/main/java/com/nodeers/todolist/MainActivity.java
@@ -1,14 +1,46 @@
 package com.nodeers.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.nodeers.todolist.AddToDo.AddTaskFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+     private FloatingActionButton fabAddTask;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fabAddTask = findViewById(R.id.addTask);
+
+        fabAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                AddTaskFragment fragment = new AddTaskFragment();
+                ft.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right);
+                //ft.replace(R.id.main, fragment, "fragment");
+                ft.replace(R.id.main,fragment,"fr");
+                ft.addToBackStack("fr");
+                fabAddTask.hide();
+                // Start the animated transition.
+                ft.commit();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        fabAddTask.show();
     }
 }
